e03eb14cccb85432532663f349bb778a7d7891bb
GuilhermeF-R <Frodriguesguilherme18@gmail.com>
2024-02-14 18:06:42 -0300

    toaster

--- src/components/new-note.tsx
+++ src/components/new-note.tsx
@@ -1,9 +1,33 @@
 import * as Dialog from '@radix-ui/react-dialog'
 import { X } from 'lucide-react'
-
+import { ChangeEvent, FormEvent, useState } from 'react'
+import {toast} from 'sonner'
 
 
 export function NewNote(){
+    const [shouldShowOnBoarding, setShouldShowOnBoarding] = useState(true)
+    const [content, setContent] = useState('')
+
+    function handleStartEditor () {
+        setShouldShowOnBoarding(false)
+    } 
+
+    function handleContentChanged(event: ChangeEvent<HTMLTextAreaElement>) {
+        setContent(event.target.value)
+
+        if (event.target.value === '') {
+            setShouldShowOnBoarding(true)
+        }
+    }
+
+    function handleSaveNote(event: FormEvent){
+        event.preventDefault()
+
+        console.log(content)
+
+        toast.success('Nota criada com sucesso!')
+    }
+
     return (
         <Dialog.Root>
             <Dialog.Trigger className=" flex flex-col  rounded-md bg-slate-700 p-5 text-left gap-3 overflow-hidden hover:ring-2 hover:ring-slate-600 focus-visible:ring-2 focus-visible:ring-lime-600 outline-none">
@@ -20,25 +44,36 @@ export function NewNote(){
             <Dialog.Portal>
                 <Dialog.Overlay className="inset-0 fixed bg-black/50">
                 <Dialog.Content className=" overflow-hidden fixed left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 max-w-[640px] w-full h-[60vh] bg-slate-700 rounded-md flex flex-col outline-none ">
+                    
                     <Dialog.Close className='absolute right-0 top-0 bg-slate-800 p-1.5 text-slate-400 hover:text-slate-100'>
                         <X className='size-5 '/>
                     </Dialog.Close>
 
-                    <div className=' flex flex-1 flex-col gap-3 p-5'>
-                        <span className=" text-sm font-medium text-slate-300 ">
-                              Adicionar nota 
-                        </span>
-                        <p className="text-sm leading-6 text-slate-400">
-                            Comece <button className=' font-medium text-lime-400 hover:underline'> gravando uma nota </button> em áudio ou se preferir <button className=' font-medium text-lime-400 hover:underline'> utilize apenas texto </button>.
-                        </p>
-                    </div>
-                    <button 
-                        type='button'
-                        className='w-full bg-lime-400 py-4 text-center text-sn text-lime-950 outline-none font-medium hover:bg-lime-500'
-                    >
-                       Salvar nota
-                    </button>
-                    
+                    <form onSubmit={handleSaveNote} className='flex-1 flex flex-col'>
+
+                        <div className=' flex flex-1 flex-col gap-3 p-5'>
+                            <span className=" text-sm font-medium text-slate-300 ">
+                                Adicionar nota 
+                            </span>
+                            {shouldShowOnBoarding ? (
+                                <p className="text-sm leading-6 text-slate-400">
+                                    Comece <button className=' font-medium text-lime-400 hover:underline'> gravando uma nota </button> em áudio ou se preferir <button onClick={handleStartEditor} className=' font-medium text-lime-400 hover:underline'> utilize apenas texto </button>.
+                            </p>
+                            ): (
+                            <textarea 
+                            autoFocus 
+                            className='text-sm leading-6 text-slate-400 bg-transparent resize-none flex-1 outline-none'
+                            onChange={handleContentChanged}/>
+                            )}
+                        </div>
+                        <button 
+                            type='submit'
+                            className='w-full bg-lime-400 py-4 text-center text-sn text-lime-950 outline-none font-medium hover:bg-lime-500'
+                            
+                        >
+                        Salvar nota
+                        </button>
+                    </form>
                 </Dialog.Content>
                 </Dialog.Overlay>
             </Dialog.Portal>
