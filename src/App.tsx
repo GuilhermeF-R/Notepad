ed3f9664576d98858ffe0d2188541600ddba439b
GuilhermeF-R <Frodriguesguilherme18@gmail.com>
2024-02-16 15:32:21 -0300

    Responsividade

--- src/App.tsx
+++ src/App.tsx
@@ -44,7 +44,7 @@ export function App() {
     : notes
 
   return  ( 
-    <div className="mx-auto max-w-6xl my-12 space-y-6">
+    <div className="mx-auto max-w-6xl my-12 space-y-6 px-5">
       <img src={logo} alt="Nlw-expert" />
 
       <form className="w-full">
@@ -57,7 +57,7 @@ export function App() {
       </form>
       <div className="h-px bg-slate-700"/>
 
-      <div className="grid grid-cols-3 auto-rows-[250px] gap-6 ">
+      <div className="grid grid-cols-1  md:grid-cols-2 lg:grid-cols-3 auto-rows-[250px] gap-6 ">
           <NewNoteCard onNoteCreated={onNoteCreated}/>
 
         {filteredNotes.map(note => {
--- src/components/new-note.tsx
+++ src/components/new-note.tsx
@@ -108,7 +108,7 @@ export function NewNoteCard({onNoteCreated}: NewNoteCardProps){
             </Dialog.Trigger>
             <Dialog.Portal>
                 <Dialog.Overlay className="inset-0 fixed bg-black/50">
-                <Dialog.Content className=" overflow-hidden fixed left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 max-w-[640px] w-full h-[60vh] bg-slate-700 rounded-md flex flex-col outline-none ">
+                <Dialog.Content className=" overflow-hidden inset-0 md:inset-auto fixed md:left-1/2 md:top-1/2 md:-translate-x-1/2 md:-translate-y-1/2 md:max-w-[640px] w-full md:h-[60vh] bg-slate-700 md:rounded-md flex flex-col outline-none ">
                     
                     <Dialog.Close className='absolute right-0 top-0 bg-slate-800 p-1.5 text-slate-400 hover:text-slate-100'>
                         <X className='size-5 '/>
